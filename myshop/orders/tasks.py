64e3118be39b0517678a29274482bec61a913415
westeastdesigns <47990949+westeastdesigns@users.noreply.github.com>
2024-06-10 23:55:54 -0400

    made minor edits to email message

--- myshop/orders/tasks.py
+++ myshop/orders/tasks.py
@@ -11,10 +11,10 @@ def order_created(order_id):
     Task to send an e-mail notification when an order is successfully created.
     """
     order = Order.objects.get(id=order_id)
-    subject = f"Order nr. {order.id}"
+    subject = f"Order number {order.id}"
     message = (
         f"Dear {order.first_name},\n\n"
-        f"You have successfully placed an order."
+        f"You have successfully placed an order. "
         f"Your order ID is {order.id}."
     )
     mail_sent = send_mail(subject, message, "admin@myshop.com", [order.email])
